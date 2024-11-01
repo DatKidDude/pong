70754ae11c089d6f0e23d4f788c296bc564f7a3a
Craig Giddings <randomhero_345@hotmail.com>
2024-11-01 18:28:42 -0400

    Added AI player class

--- app.py
+++ app.py
@@ -28,30 +28,29 @@ class Ball:
         """Move the ball around"""
         self.ball_obj.move_ip(self.speed)
 
-        if self.ball_obj.right >= self.board.board_rect.right or self.ball_obj.left < 0:
-            self.speed.x = -self.speed.x 
+        # if self.ball_obj.right >= self.board.board_rect.right or self.ball_obj.left < 0:
+        #     self.speed.x = -self.speed.x 
         
         if self.ball_obj.bottom >= self.board.board_rect.bottom or self.ball_obj.top < 0:
             self.speed.y = -self.speed.y
         
         pygame.draw.circle(self.board.board_surface, self.color, self.ball_obj.center, self.radius)
 
-
 class Paddle:
     """A paddle to hit the ball"""
     def __init__(self, board, color, x, y):
+        # Gameboard object
         self.board = board
         self.color = color
         self.x = x
         self.y = y
-        self.offset = 10
         self.speed = 20
 
         # Create the paddle
         self.paddle = pygame.Rect(self.x, self.y, 10, 70)
         # self.paddle_rect = pygame.draw.rect(self.board.board_surface, self.color, self.paddle, 0, 2)
         self.moving = False
-    
+
 
     def update(self):
         """Move the paddle"""
@@ -61,9 +60,21 @@ class Paddle:
             self.paddle.move_ip(0, self.speed)
         elif keys[pygame.K_UP] and self.paddle.top > 0:
             self.paddle.move_ip(0, -self.speed)
-        
+
         pygame.draw.rect(self.board.board_surface, self.color, self.paddle, 0, 2)
-        
+
+
+class AI(Paddle):
+    def __init__(self, board, color, x, y, ball):
+        super().__init__(board, color, x, y)
+        self.ball = ball
+
+    def update(self):
+        """Have the computer follow the ball"""
+        self.paddle.bottom = self.ball.ball_obj.bottom 
+
+        pygame.draw.rect(self.board.board_surface, self.color, self.paddle, 0, 2)
+
 class GameBoard:
     """An outline of the playable game area"""
     def __init__(self, screen, color):
@@ -93,6 +104,12 @@ class GameBoard:
         pygame.draw.line(self.board_surface, self.color, (self.board_surface_rect.width / 2, 0), (self.board_surface_rect.width / 2, self.board_surface_rect.height))
         
 
+def hit_ball(player, ai_player, ball):
+    """Check if player or ai has hit the ball"""
+    if player.paddle.colliderect(ball.ball_obj) or ai_player.paddle.colliderect(ball.ball_obj):
+        ball.speed.x = -ball.speed.x
+
+
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 600
 FPS = 60
@@ -110,8 +127,9 @@ clock = pygame.time.Clock()
 # Create the gameboard
 gameboard = GameBoard(screen, WHITE)
 # Create the player
-player = Paddle(gameboard, GREEN, 10, 0)
 ball = Ball(gameboard, "maroon")
+player = Paddle(gameboard, GREEN, 10, 0)
+ai_player = AI(gameboard, GREEN, 830, 0, ball)
 
 running = True
 while running:
@@ -120,7 +138,9 @@ while running:
             running = False
 
     player.update()
+    ai_player.update()
     ball.update()
+    hit_ball(player, ai_player, ball)
     gameboard.render_board()
 
     # Draw everything on the screen
